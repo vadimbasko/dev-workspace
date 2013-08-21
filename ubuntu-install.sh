2f980b4c4d8850bda1c03c499643725c2b44a4bd
vadim <vadim@vadimbuk.(none)>
2013-08-22 00:37:24 +0400

    start

---
+++ ubuntu-install.sh
@@ -0,0 +1,32 @@
+#apt-get autoremove
+#apt-get purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy onboard
+
+#add-apt-repository ppa:diodon-team/stable
+#apt-get update
+#apt-get install diodon
+
+#apt-add-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+#apt-get update
+#apt-get install skype
+#apt-get purge fonts-nanum
+
+#apt-get install mc diodon openssh-server xrdp
+
+#apt-get install gxneur
+#gsettings set com.canonical.Unity.Panel systray-whitelist "['all']"
+
+#apt-get install openvpn
+#mkdir /etc/openvpn/keys
+
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+#sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#apt-get update
+#apt-get install google-chrome-stable
+
+#-----------       Programmer
+
+apt-get install git 
+
+mkdir /prj
+chmod 777 /prj
+mkdir /prj/ubuntu
