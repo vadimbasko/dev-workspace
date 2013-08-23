c270c07ba221cd8e6f9b8ace3fa204fc9751e3bc
vadim <vadim@basko2.(none)>
2013-08-23 13:23:44 +0400

    !

---
+++ ubuntu-install-dev.sh
@@ -0,0 +1,4 @@
+apt-get install shutter subversion 
+
+apt-get install openvpn
+mkdir /etc/openvpn/keys
--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -3,32 +3,27 @@
 #git commit
 #git push
 
-#apt-get autoremove
-#apt-get purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy onboard
+apt-get autoremove
+apt-get purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy onboard
 
-#add-apt-repository ppa:diodon-team/stable
-#apt-get update
-#apt-get install diodon
+add-apt-repository ppa:diodon-team/stable
+apt-get update
+apt-get install diodon
 
-#apt-add-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-#apt-get update
-#apt-get install skype
-#apt-get purge fonts-nanum
+apt-add-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+apt-get update
+apt-get install skype
+apt-get purge fonts-nanum
 
-#apt-get install mc diodon openssh-server xrdp
+apt-get install mc openssh-server xrdp
 
-#apt-get install gxneur aspell-ru
-#gsettings set com.canonical.Unity.Panel systray-whitelist "['all']"
+apt-get install gxneur aspell-ru
+gsettings set com.canonical.Unity.Panel systray-whitelist "['all']"
 
-#apt-get install openvpn
-#mkdir /etc/openvpn/keys
-
-#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-#sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-#apt-get update
-#apt-get install google-chrome-stable
-
-#-----------       Programmer
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get install google-chrome-stable
 
 apt-get install git 
 
