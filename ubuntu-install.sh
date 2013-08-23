11fe665ed489408c0366f6239791b88035c4b853
vadim <vadim@vadimbuk.(none)>
2013-08-23 23:28:01 +0400

    q

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,34 +4,25 @@
 #git push
 #git pull origin master 
 
-apt-get autoremove
-apt-get purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy onboard
+#mkdir /prj
+#chmod 777 /prj
+#mkdir /prj/ubuntu
 
-add-apt-repository ppa:diodon-team/stable
-apt-get update
-apt-get install diodon
+apt-get -y autoremove
+apt-get -y purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy onboard
 
-apt-add-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-apt-get update
-apt-get install skype
-apt-get purge fonts-nanum
-
-add-apt-repository ppa: webupd8team/sublime-text-3
-apt-get update
-apt-get install sublime-text-installer
-
-apt-get install mc openssh-server xrdp
-
-apt-get install gxneur aspell-ru
-gsettings set com.canonical.Unity.Panel systray-whitelist "['all']"
-
+#skype repository
+add-apt-repository --yes "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+#google-chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+add-apt-repository --yes ppa:diodon-team/stable webupd8team/sublime-text-3
 apt-get update
-apt-get install google-chrome-stable
 
-apt-get install git 
+apt-get -y install skype diodon sublime-text-installer git gitk mc openssh-server xrdp gxneur aspell-ru google-chrome-stable
+apt-get -y purge fonts-nanum
 
-mkdir /prj
-chmod 777 /prj
-mkdir /prj/ubuntu
+#configuration - show xneural icon on tray
+gsettings set com.canonical.Unity.Panel systray-whitelist "['all']"
+
