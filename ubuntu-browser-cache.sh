9c7b99817117e6b417b16fd07fdcef5afba1de95
vadimbasko <vadimbasko@users.noreply.github.com>
2013-10-12 12:33:31 +0400

    Update ubuntu-browser-cache.sh

--- ubuntu-browser-cache.sh
+++ ubuntu-browser-cache.sh
@@ -1,3 +1,5 @@
 #firefox cache
 cd /home/vadim/.cache/mozilla/firefox/i2hjctik.default/Cache 
-find -size +1M -exec cp {} ~/Downloads/muz \;
+find -size +1M -exec cp {} ~/Downloads/muz/album \;
+
+rename s/'$'/'.mp3'/ ~/Downloads/muz/album/*
