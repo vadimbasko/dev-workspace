5e1c7569a70039efb378b11c56b89839e932915d
vadim <vadim@basko2.(none)>
2013-10-15 12:41:10 +0400

    --allow-empty

--- browser-cache.sh
+++ browser-cache.sh
@@ -1,5 +1,6 @@
 #firefox cache test 2 3
-cd /home/vadim/.cache/mozilla/firefox/i2hjctik.default/Cache 
+cd ~/.cache/mozilla/firefox/i2hjctik.default/Cache 
+touch ~/Downloads/muz/album
 find -size +1M -exec cp {} ~/Downloads/muz/album \;
 
 rename s/'$'/'.mp3'/ ~/Downloads/muz/album/*
