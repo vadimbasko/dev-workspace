6251b94ff4742979d053005ecac9c1b5f5b0b0c8
vadim <vadim@vadimbuk.(none)>
2013-10-15 22:25:18 +0400

    1

--- install-dev.sh
+++ install-dev.sh
@@ -1,7 +1,14 @@
-apt-get install shutter subversion 
+apt-get -y install shutter subversion 
 
-apt-get install openvpn
+apt-get -y install openvpn
 mkdir /etc/openvpn/keys
+
+# then copy ovpn key to /etc/openvpn/keys
+
+#cd /etc/openvpn
+#sudo openvpn --config basko.ovpn 
+
+apt-get -y autoremove
 
 #todo idea
 #todo gradle
