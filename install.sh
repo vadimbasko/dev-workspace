8d40d7f960f3ce0ca7e9d53700bcb19fe2558be0
vadim <vadim@vadimbuk.(none)>
2013-10-20 14:08:33 +0400

    1

--- install.sh
+++ install.sh
@@ -17,18 +17,30 @@ add-apt-repository --yes ppa:jon-severinsson/ffmpeg
 apt-get update
 
 apt-get -y install skype diodon sublime-text-installer git gitk gxneur aspell-ru google-chrome-stable
-#skype fonts fix
+
+echo 'install skype'
+apt-get -y install skype
+echo 'skype fonts fix - remove fonts-nanum'
 apt-get -y purge fonts-nanum  
 
 #add sublimetext to path
 export PATH=$PATH:/opt/sublime_text
 
-#file manager, system settings required for switch off sound
+echo 'install file manager krusader, system settings required for switch off sound'
 apt-get -y install krusader, systemsettings
 
 apt-get -y install vlc openshot ffmpeg hugin wine winetricks gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad
 
 apt-get -y install vnstat mc openssh-server xrdp
 
-#configuration - show xneural icon on tray
+echo 'configuration - show xneural icon on tray'
 gsettings set com.canonical.Unity.Panel systray-whitelist "['all']"
+
+echo 'disable login sounds'
+mv /usr/share/sounds/ubuntu/stereo/system-ready{,-disabled}.ogg
+mv /usr/share/sounds/ubuntu/stereo/desktop-login{,-disabled}.ogg
+
+echo 'asus keyboard backlight - switch off on startup'
+touch /etc/init/keyboard-brightness.conf 
+chmod 777 /etc/init/keyboard-brightness.conf
+echo -e "#switch off keyboard brightness on start up \necho 0 | tee /sys/class/leds/asus::kbd_backlight/brightness" > /etc/init/keyboard-brightness.conf 
