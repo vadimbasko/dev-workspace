3ccc3ee1717d40f2f5973f3fb204f05050f5671b
vadim <vadim@vadimbuk.(none)>
2013-10-26 10:19:21 +0400

    1

--- install-dev.sh
+++ install-dev.sh
@@ -4,16 +4,14 @@ apt-get -y install oracle-java7-installer
 
 apt-get -y install openvpn
 mkdir /etc/openvpn/keys
-
 # then copy ovpn key to /etc/openvpn/keys
 
+#start openvpn
 #cd /etc/openvpn
 #setsid sudo openvpn --config basko.ovpn 
 
 #check client is start
 #sudo ps -x | grep openvpn
-
-
 
 apt-get -y install shutter subversion 
 
@@ -21,3 +19,7 @@ apt-get -y autoremove
 
 #todo idea
 #todo gradle
+
+echo 'add idea and gradle path to ~/.bashrc'
+echo -e "\n export PATH=\$PATH:/opt/gradle/bin:/opt/idea/bin" >> ~/.bashrc
+echo -e "\n export GRADLE_HOME=/opt/gradle" >> ~/.bashrc
