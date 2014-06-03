b0c999deb3797cfc0c411adb3f84f16ac9f62604
vadimbasko <vadimbasko@users.noreply.github.com>
2014-06-03 16:31:06 +0400

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -20,7 +20,7 @@ apt-get -y autoremove
 #todo idea
 
 #todo gradle
-wget -P /opt/gradle https://services.gradle.org/distributions/gradle-1.12-all.zip
+wget -P /opt https://services.gradle.org/distributions/gradle-1.12-all.zip
 
 
 echo 'add idea and gradle path to ~/.profile'
