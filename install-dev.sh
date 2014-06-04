5751e35bd4a133210a670818b0268b3e9f0e92d5
vadimbasko <vadimbasko@users.noreply.github.com>
2014-06-04 15:58:26 +0400

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -27,7 +27,7 @@ echo -e "\n export GRADLE_HOME=/opt/gradle" >> ~/.profile
 
 wget http://download.jetbrains.com/idea/ideaIU-12.1.7b.tar.gz -P /opt
 tar -xf /opt/ideaIU-12.1.7b.tar.gz -C /opt
-
+ mv /opt/idea-IU-129.1525/ /opt/idea
 
 echo 'add idea and gradle path to ~/.profile'
 echo -e "\n export PATH=\$PATH:/opt/gradle/bin:/opt/idea/bin" >> ~/.profile
