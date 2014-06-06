decddc4c86e0c3919f9f8d6e5ad2063ebb9f097d
vadimbasko <vadimbasko@users.noreply.github.com>
2014-06-06 11:36:10 +0400

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -24,10 +24,11 @@ wget -P /opt https://services.gradle.org/distributions/gradle-1.12-all.zip
 unzip /opt/gradle-1.12-all.zip -d /opt
 mv /opt/gradle-1.12 /opt/gradle
 echo -e "\n export GRADLE_HOME=/opt/gradle" >> ~/.profile
+echo -e "\n export PATH=\$PATH:/opt/gradle/bin:/opt/idea/bin" >> ~/.profile
+
 
 wget http://download.jetbrains.com/idea/ideaIU-12.1.7b.tar.gz -P /opt
 tar -xf /opt/ideaIU-12.1.7b.tar.gz -C /opt
- mv /opt/idea-IU-129.1525/ /opt/idea
+mv /opt/idea-IU-129.1525/ /opt/idea
 
-echo 'add idea and gradle path to ~/.profile'
-echo -e "\n export PATH=\$PATH:/opt/gradle/bin:/opt/idea/bin" >> ~/.profile
+
