9b3226149eeed87c26036be1e5191ef0d093b1d2
vadimbasko <vadimbasko@users.noreply.github.com>
2014-12-19 13:55:06 +0300

    Update and rename log.sh to install-12-14.sh

---
+++ install-12-14.sh
@@ -0,0 +1,10 @@
+#skype tray icon
+sudo apt-get install sni-qt:i386
+
+# shutdown window command - gnome-session-quit --power-off --force
+
+add-apt-repository --yes ppa:diodon-team/stable
+add-apt-repository --yes ppa:webupd8team/sublime-text-3
+apt-get update
+
+apt-get -y install diodon sublime-text-installer git 
--- log.sh
+++
@@ -1,4 +0,0 @@
-#skype tray icon
-sudo apt-get install sni-qt:i386
-
-# shutdown window command - gnome-session-quit --power-off --force
