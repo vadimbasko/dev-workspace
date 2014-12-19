d0fd63ea955f90eec85a4e05851c26f1eb310fb6
vadimbasko <vadimbasko@users.noreply.github.com>
2014-12-19 14:29:43 +0300

    Update install-12-14.sh

--- install-12-14.sh
+++ install-12-14.sh
@@ -40,4 +40,4 @@ sed -i.bak s/gyazo.com/screenshot.haulmont.com/g /usr/bin/gyazo
 sed -i.bak s/upload.cgi/upload.php/g /usr/bin/gyazo
 
 #git credentials for a year
-git config --global credential.helper 'cache --timeout=604800'
+git config --global credential.helper 'cache --timeout=31536000'
