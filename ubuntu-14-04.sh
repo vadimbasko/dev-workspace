80f0319d870d71658753c0bfd9ac744345ba884f
vadimbasko <vadimbasko@users.noreply.github.com>
2015-04-16 21:06:30 +0300

    Update ubuntu-14-04.sh

--- ubuntu-14-04.sh
+++ ubuntu-14-04.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install skype compizconfig-settings-manager gthumb mc
+sudo apt-get -y install skype compizconfig-settings-manager gthumb mc vlc
 
 #stand by sda disk
 #sudo hdparm -B -Y /dev/sda
