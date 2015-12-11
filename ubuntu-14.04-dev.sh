a284cf8e172a7b63f8344be5bdb9a2e21e0b98e4
vadimbasko <vadimbasko@users.noreply.github.com>
2015-12-11 12:04:55 +0300

    Update ubuntu-14.04-dev.sh

--- ubuntu-14.04-dev.sh
+++ ubuntu-14.04-dev.sh
@@ -6,3 +6,11 @@ unzip /opt/gradle-1.12-all.zip -d /opt
 mv /opt/gradle-1.12 /opt/gradle
 echo -e "\nexport GRADLE_HOME=/opt/gradle" >> ~/.profile
 echo -e "\nexport PATH=\$PATH:/opt/gradle/bin:/opt/idea/bin" >> ~/.profile
+
+#mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse"
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
