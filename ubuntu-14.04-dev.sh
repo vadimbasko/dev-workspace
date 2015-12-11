f451c0c38f8a1713adbae203e75012eeaf44b2e9
vadimbasko <vadimbasko@users.noreply.github.com>
2015-12-11 12:10:47 +0300

    Update ubuntu-14.04-dev.sh

--- ubuntu-14.04-dev.sh
+++ ubuntu-14.04-dev.sh
@@ -9,8 +9,9 @@ echo -e "\nexport PATH=\$PATH:/opt/gradle/bin:/opt/idea/bin" >> ~/.profile
 
 #mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse"
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
 sudo apt-get update
 
+#mongodb
 sudo apt-get install -y mongodb-org
