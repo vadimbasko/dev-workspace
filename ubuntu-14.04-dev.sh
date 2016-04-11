79600096245a78bab974bc7853d41ecfc7457d27
vadimbasko <vadimbasko@users.noreply.github.com>
2016-04-11 10:11:49 +0400

    Update ubuntu-14.04-dev.sh

--- ubuntu-14.04-dev.sh
+++ ubuntu-14.04-dev.sh
@@ -16,6 +16,12 @@ sudo apt-get update
 #mongodb
 sudo apt-get install -y mongodb-org
 
+#mono
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian multiverse" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+
 #ssh keys permissions
 sudo chmod 600  ~/.ssh/keys/xxx/id_rsa
 sudo chmod 600  ~/.ssh/keys/xxx/id_rsa.pub 
