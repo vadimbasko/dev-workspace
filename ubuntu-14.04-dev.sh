368aac9f754983dddfd34daf05ef020229438975
vadimbasko <vadimbasko@users.noreply.github.com>
2016-04-11 10:41:39 +0400

    Update ubuntu-14.04-dev.sh

--- ubuntu-14.04-dev.sh
+++ ubuntu-14.04-dev.sh
@@ -21,6 +21,8 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
+sudo apt-get install mono-complete
+
 
 #ssh keys permissions
 sudo chmod 600  ~/.ssh/keys/xxx/id_rsa
