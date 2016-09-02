d771c55a79428aad19729024eff519f92e44caec
vadimbasko <vadimbasko@users.noreply.github.com>
2016-09-02 12:32:46 +0400

    Update ubuntu-14.04-dev.sh

--- ubuntu-14.04-dev.sh
+++ ubuntu-14.04-dev.sh
@@ -16,6 +16,11 @@ sudo apt-get update
 #mongodb
 sudo apt-get install -y mongodb-org
 
+#old node
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+nvm install 0.10.33
+nvm alias default 0.10.33
+
 #mono
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
