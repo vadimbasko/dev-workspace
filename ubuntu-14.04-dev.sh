606c7dbc2c03874320a713b24006a4a5ec68aeea
vadimbasko <vadimbasko@users.noreply.github.com>
2016-09-10 15:02:54 +0400

    Update ubuntu-14.04-dev.sh

--- ubuntu-14.04-dev.sh
+++ ubuntu-14.04-dev.sh
@@ -40,3 +40,13 @@ sudo apt-get install nodejs=0.10.25~dfsg2-2ubuntu1
 #mysql
 sudo apt-get install mysql-server
 
+#virtualbox
+#Add the following line to your /etc/apt/sources.list:
+deb http://download.virtualbox.org/virtualbox/debian xenial contrib
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install virtualbox-5.1
+
