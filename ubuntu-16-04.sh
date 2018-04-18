5dd2cdc51c9f5f7941572c0ee85971f26699a69d
vadimbasko <vadimbasko@users.noreply.github.com>
2018-04-18 23:49:36 +0400

    Update ubuntu-16-04.sh

--- ubuntu-16-04.sh
+++ ubuntu-16-04.sh
@@ -2,10 +2,6 @@
 
 # drivers
 
-#huawei modem fix
-#sudo usb_modeswitch -v 12d1 -p 1f01 -J 
-#lsusb 
-
 #tools
 sudo apt install -y mc
 
@@ -16,3 +12,11 @@ sudo apt install -y copyq
 
 sudo add-apt-repository ppa:webupd8team/java && sudo apt update
 sudo apt install oracle-java8-installer
+
+#touchpad
+synclient PalmDetect=1
+
+#huawei modem
+lsusb
+#Bus 001 Device 004: ID 12d1:1f01 Huawei Technologies Co., Ltd. E353/E3131 (Mass storage mode)
+sudo usb_modeswitch -v 12d1 -p 1f01 -J
