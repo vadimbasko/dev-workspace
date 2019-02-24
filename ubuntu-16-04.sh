2a7af3ac0b72696d358df964d101ec01f43e7868
vadimbasko <vadimbasko@users.noreply.github.com>
2019-02-24 08:43:27 +0400

    Update ubuntu-16-04.sh

--- ubuntu-16-04.sh
+++ ubuntu-16-04.sh
@@ -59,5 +59,8 @@ git clone https://github.com/lwfinger/rtlwifi_new.git
 cd rtlwifi_new
 make
 sudo make install
+sudo su -
+echo "options rtl8723be ant_sel=2" > /etc/modprobe.d/50-rtl8723be.conf
+exit
 sudo modprobe -r rtl8723be
 sudo modprobe rtl8723be
