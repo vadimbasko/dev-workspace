3a40c35eaf76fb2ecd2226ad0b13a81057d0c7f2
Vadim Basko <vadimbasko@users.noreply.github.com>
2021-02-28 14:28:51 +0400

    Update 20-04.sh

--- 20-04.sh
+++ 20-04.sh
@@ -1,6 +1,12 @@
 # fix PCIe error 
 # https://askubuntu.com/questions/771899/pcie-bus-error-severity-corrected
 
+# postgres
+# https://www.postgresql.org/download/linux/ubuntu/
+
+# jdk
+# https://adoptopenjdk.net/installation.html#linux-pkg
+
 # apt update 
 sudo apt update
 # update ubuntu
@@ -17,4 +23,10 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 nvm install 14.16.0
 npm i -g lerna
 
+# config
 
+# autohide toopbar 
+# https://ubuntuhandbook.org/index.php/2020/08/top-panel-auto-hide-ubuntu-20-04/
+
+# show clocks on topbar
+gsettings set org.gnome.desktop.interface clock-show-seconds true
