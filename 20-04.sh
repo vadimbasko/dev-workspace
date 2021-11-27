33116f2a5b8aea01101bcb5019cc122bbbf9fe75
Vadim Basko <vadimbasko@users.noreply.github.com>
2021-11-27 21:24:44 +0400

    Update 20-04.sh

--- 20-04.sh
+++ 20-04.sh
@@ -28,6 +28,11 @@ npm i -g lerna
 
 # jdk
 # https://adoptopenjdk.net/installation.html#linux-pkg
+# https://askubuntu.com/questions/1157417/install-adoptopenjdk-onto-ubuntu-18-04-lts
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+sudo apt update
+sudo apt install -y adoptopenjdk-11-hotspot
 
 # config
 
