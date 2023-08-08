92894010380afdcf0f8ec50a5c6be2f0798b979b
Vadim Basko <vadimbasko@users.noreply.github.com>
2023-08-08 12:11:41 +0400

    Update 20-04.sh

--- 20-04.sh
+++ 20-04.sh
@@ -24,6 +24,15 @@ sudo apt install peek -y
 sudo wget -P /opt https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i /opt/google-chrome-stable_current_amd64.deb
 
+# pinta
+sudo apt install ca-certificates gnupg
+sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo add-apt-repository ppa:pinta-maintainers/pinta-stable
+sudo apt update
+sudo apt install mono-devel --yes
+sudo apt install pinta --yes
+
 # development
 
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
